ccf1c5785bcc3e6124d13d15cc6bb3974f50affc
Jacob Janak <jakeathotmail@gmail.com>
2018-06-11 19:53:48 -0700

    Made the site mobile-responsive

--- ceramics/views.py
+++ ceramics/views.py
@@ -20,8 +20,6 @@ def events(request):
     return render(request, 'ceramics/events.html', {'active_link': 'events'})
 
 def product(request):
-    print(request.path)
     item_id = request.path.strip('store/item/')
-    print(item_id)
     product = Item.objects.get(id=item_id)
     return render(request, 'ceramics/product.html', {'active_link': 'store', 'product': product})
